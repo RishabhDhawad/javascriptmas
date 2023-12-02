47e3c64e84251dd16a8298e57ac6608659299657
RishabhDhawad <rishabhdhawad29@gmail.com>
2023-12-02 17:27:31 +0530

    Day 1

--- Day1.js
+++ Day1.js
@@ -1,26 +1,13 @@
-// Function to calculate and show the countdown
-function renderCountdown() {
-    // Set the day for Christmas
-    const christmasDay = 25;
+const countdownDisplay = document.getElementById("countdown-display");
 
-    // Get today's date
+function renderCountdown(){
+    const christmas = new Date('December 25, 2023').getDate();
+    // Task:
+    // - Get today's date (you only need the day).
     const today = new Date();
-    const todayDay = today.getDate();
-
-    // Calculate remaining days until Christmas
-    let remainingDays;
-    if (todayDay <= christmasDay) {
-        remainingDays = christmasDay - todayDay;
-    } else {
-        // If Christmas has passed this year, calculate for next year
-        const nextChristmas = new Date(today.getFullYear() + 1, 11, christmasDay);
-        const daysUntilNextChristmas = Math.ceil((nextChristmas - today) / (1000 * 60 * 60 * 24));
-        remainingDays = daysUntilNextChristmas;
-    }
-
-    // Display remaining days
-    console.log(`Days 'til Christmas! ${remainingDays} days`);
-    
+    // - Calculate remaining days.
+    return christmas - today.getDate();
+    // - Display remaining days in countdownDisplay.
 }
-// Call the function to render the countdown
-renderCountdown();
+ 
+countdownDisplay.textContent = renderCountdown()
